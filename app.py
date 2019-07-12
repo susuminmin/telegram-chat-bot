81d1b230d42cb003511b041c61b0f66097c7a4a9
susuminmin <mytoutbon@gmail.com>
2019-07-12 16:40:39 +0900

    Implement Translation Functionality Using NAVER Papago

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, escape, request
 from decouple import config 
 import pprint
 import requests
-
 app = Flask(__name__)
+
+
 API_TOKEN = config('API_TOKEN')     # 상수는 대문자
+NAVER_CLIENT_ID = config('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = config('NAVER_CLIENT_SECRET')
 
 
 @app.route('/')
@@ -26,8 +29,47 @@ def telegram():
         chat_id = from_telegram.get('message').get('chat').get('id')
         text = from_telegram.get('message').get('text')
 
-        if text == '점심메뉴':
-            text = '짜장면이나 먹어'
+        # 첫 네글자가 '/번역 '일 때 
+    if  text[0:4] == '/한영 ':
+        headers = {
+            'X-Naver-Client-Id': NAVER_CLIENT_ID,
+            'X-Naver-Client-Secret': NAVER_CLIENT_SECRET, # trailing comma
+        } 
+        data = {
+            'source': 'ko',
+            'target': 'en',
+            'text': text[4:] # /번역 이후 텍스트들만 번역하겠다 
+        } 
+        papago_url = 'https://openapi.naver.com/v1/papago/n2mt' 
+        papago_res = requests.post(papago_url, headers=headers, data=data)
+        text = papago_res.json().get('message').get('result').get('translatedText')
+            
+        base_url = 'https://api.telegram.org'
+        api_url = f'{base_url}/bot{API_TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
+
+        print('chat_id : ', chat_id)
+        print('text : ', text)
+        requests.get(api_url)
+    
+
+
+
+    if  text[0:4] == '/영한 ':
+        headers = {
+            'X-Naver-Client-Id': NAVER_CLIENT_ID,
+            'X-Naver-Client-Secret': NAVER_CLIENT_SECRET, # trailing comma
+        } 
+        data = {
+            'source': 'en',
+            'target': 'ko',
+            'text': text[4:] # /번역 이후 텍스트들만 번역하겠다 
+        } 
+        papago_url = 'https://openapi.naver.com/v1/papago/n2mt' 
+        papago_res = requests.post(papago_url, headers=headers, data=data)
+        text = papago_res.json().get('message').get('result').get('translatedText')
+
+
+
 
         # Send Message API URL
         base_url = 'https://api.telegram.org'
